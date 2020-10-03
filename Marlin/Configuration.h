72eb8b379d97a080e1dee101c09f44467a0ce212
mrh3nk <63805052+mrh3nk@users.noreply.github.com>
2020-10-03 17:07:29 +0200

    Update Configuration.h
    Updated esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -735,7 +735,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 1600, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 1600, 1650 }
 
 /**
  * Default Max Feed Rate (mm/s)
